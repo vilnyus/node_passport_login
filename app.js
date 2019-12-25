f954ad3d25988c80a727cf42c9b1133234998b91
vilnyus <govagharsh@gmail.com>
2019-12-25 16:11:11 +0400

    layout and styles

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express');
-
+const expressLayouts = require('express-layouts');
 const app = express();
+
+app.use(express.static(__dirname + '/'));
+// app.use(express.static(__dirname + '/public'));
+
+// EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
 
 // Router
 app.use('/', require('./routes/index'));
-app.get('/details', (req, res)=>res.send("Year: 2020"));
+app.use('/users', require('./routes/users'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server started on port ${PORT}.`));
