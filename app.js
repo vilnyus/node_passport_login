6e5d7c318edd5130c70c4e62e2ed35b34fbbd0e3
vilnyus <govagharsh@gmail.com>
2019-12-27 15:53:09 +0400

    EJS compleated

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const express = require('express');
-const expressLayouts = require('express-layouts');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+
 const app = express();
 
-app.use(express.static(__dirname + '/'));
-// app.use(express.static(__dirname + '/public'));
+// DB Config
+const db = require('./config/keys').MongoURI;
+
+// Connect to Mongo
+mongoose.connect(db, { useNewUrlParser: true})
+    .then(()=>console.log('MongoDB Connected...'))
+    .catch(err=>console.log(err));
+    
+// app.use(express.static(__dirname + '/'));
 
 // EJS
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: true }));
 
 // Router
 app.use('/', require('./routes/index'));
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MongoURI: 'mongodb://user-abc:user123@ds353748.mlab.com:53748/users_db_auth'
+}
