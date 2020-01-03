a0af101766c9d951c769879326a0a87845de06d3
vilnyus <govagharsh@gmail.com>
2020-01-03 16:13:18 +0400

    partials

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 const db = require('./config/keys').MongoURI;
 
 // Connect to Mongo
-mongoose.connect(db, { useNewUrlParser: true})
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(()=>console.log('MongoDB Connected...'))
     .catch(err=>console.log(err));
     
