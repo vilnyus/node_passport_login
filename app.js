c3b1dc0787d08f660d9b9004eb824422bbde26f1
vilnyus <govagharsh@gmail.com>
2020-01-07 14:20:06 +0400

    registration done

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
-// const User = require('../models/User');
+const flash = require('connect-flash');
+const session = require('express-session');
+// const User = require('./models/User');
 
 const app = express();
 
@@ -21,6 +23,23 @@ app.set('view engine', 'ejs');
 
 // Bodyparser
 app.use(express.urlencoded({ extended: true }));
+
+// Express session
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+
+// Connect flash
+app.use(flash());
+
+// Global Vars
+app.use((req, res, next)=>{
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    next();
+});
 
 // Router
 app.use('/', require('./routes/index'));
